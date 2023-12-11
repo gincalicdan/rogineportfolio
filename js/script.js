03a02d58eb3a7d421b9d64da39340477cb1908e5
gincalicdan <149605935+gincalicdan@users.noreply.github.com>
2023-12-10 21:13:34 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,4 +13,20 @@
 // Call the function when the page is loaded
 window.onload = targetSpecificId;
 
+const vibes = document.getElementById("joke");
+const btn = document.getElementById("btn");
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single";
+
+let myVibe = () => {
+
+    fetch(url)
+    .then(data => data.json())
+    .then(item =>{
+        vibes.textContent = `${item.joke}`;
+    });
+}
+
+btn.addEventListener("click", myVibe);
+myVibe();
+
 
